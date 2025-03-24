29fcfed39d51a40319d05903ef0c5e2dd1a6b77c
Zenyi <zenyigom@hotmail.com>
2025-03-24 19:02:17 +0000

    Add windows 97 style

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,13 +1,13 @@
 'use client';
 
-
 import { useFormStatus } from 'react-dom';
+import classes from './meals-form-submit.module.css';
 
 export default function MealsFormSubmit() {
     const { pending } = useFormStatus();
 
     return (
-        <button type="submit" disabled={pending}>
+        <button className={classes.button} type="submit" disabled={pending}>
             {pending ? 'Saving...' : 'Share Meal'}
         </button>
     );
